0834e29fdeba71395f79bffaf5c99c0c30ddacb7
przemek625 <przemek625@gmail.com>
2018-06-21 13:51:41 +0200

    Add error handling

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+import { Observable } from 'rxjs/Observable';
+import { Key } from 'protractor';
 
 
 @Injectable()
@@ -19,16 +24,38 @@ login(model: any) {
             localStorage.setItem('token', user.tokenString);
             this.userToken = user.tokenString;
         }
-    });
+    }).catch(this.handleError);
 }
 
 register(model: any) {
-    return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+    return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
 }
 
 private requestOptions() {
     const headers = new Headers({'Content-Type': 'application/json'});
     return new RequestOptions({headers: headers});
+}
+
+private handleError(error: any) {
+    const applicationError = error.headers.get('Application-Error');
+
+    if (applicationError) {
+        return Observable.throw(applicationError);
+    }
+    const serverError = error.json();
+    let modelStateErrors = '';
+    if (serverError) {
+        for (const key in serverError) {
+            if (serverError[key]) {
+                modelStateErrors += serverError[key] + '\n';
+            }
+        }
+    }
+    return Observable.throw(
+        modelStateErrors || 'Server error'
+    );
+
+
 }
 
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -20,6 +20,7 @@ export class NavComponent implements OnInit {
 
     }, error => {
       console.log('failed to log in');
+      console.log(error);
     });
     console.log(this.model);
   }
