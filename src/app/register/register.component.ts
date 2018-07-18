9c634ff0f935a7f19afbbc530bb44e5c2f5a06fb
przemek625 <przemek625@gmail.com>
2018-07-18 14:01:53 +0200

    Add new Registation implementation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,6 +8,8 @@ import {
   FormBuilder
 } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -18,14 +20,15 @@ export class RegisterComponent implements OnInit {
   // https://angular.io/guide/template-syntax#inputs-outputs
   @Output() cancelRegister = new EventEmitter();
 
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
   constructor(
     private authService: AuthService,
     private alertify: AlertifyService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -65,16 +68,22 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.model);
-    this.authService.register(this.model).subscribe(
-      () => {
-        console.log('Registratiob succsessful');
-        this.alertify.success('Registratiob succsessful');
-      },
-      error => {
-        console.log(error);
-      }
-    );
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(
+        () => {
+          this.alertify.success('Registration successful');
+        },
+        error => {
+          this.alertify.error(error);
+        },
+        () => {
+          this.authService.login(this.user).subscribe(() => {
+            this.router.navigate(['/members']);
+          });
+        }
+      );
+    }
   }
 
   cancel() {
